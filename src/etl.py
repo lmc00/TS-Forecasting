c394b236ba86e8f135d4480e7b6455744c96cfff
WR3H2ep+v6K62RM7eEa9JACNU9OWkfCbqFKguuYm46E= <lmc00unican@gmail.com>
2023-08-06 22:35:44 +0200

    First ACF, PACF, ADF studies and graphs

--- src/etl.py
+++ src/etl.py
@@ -1,122 +1,44 @@
-## Imports
-from pyspark.sql import SparkSession
-from pyspark import SparkContext
+# imports and useful variables
+import sys
 import os
-import copy
-import time
-# import statsmodels
-import pandas as pd
 import numpy as np
+import pandas as pd
+import dask as dd
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import adfuller, kpss, acf, grangercausalitytests
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf, month_plot, quarter_plot
+from scipy import signal
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Spark dependencies
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from pyspark import StorageLevel, SparkConf
-from pyspark.ml.feature import StandardScaler, VectorAssembler, PCA
-from pyspark.mllib.linalg import SparseVector, DenseVector, VectorUDT
-from pyspark.ml.classification import LogisticRegression
+from statsmodels.tsa.stattools import adfuller
+import statsmodels.api as sm
+from pylab import rcParams
 
-from pyspark.sql import functions as F
-from pyspark.sql.window import Window
-from datetime import datetime, timedelta
-from pyspark.sql.types import TimestampType
-from pyspark.sql.functions import col, expr, udf, sequence
+from datetime import timedelta
+# from statsmodels.tsa.forecasting.stl import STLForecast
+import numpy as np
+import pandas as pd
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+import pickle
 
-if __name__ == "__main__":
-    spark = SparkSession.builder.appName("Example_overflow").getOrCreate()
+if __name__ == '__main__':
+    spark = SparkSession \
+        .builder \
+        .appName('Training Sarimax') \
+        .getOrCreate()
     sc = spark.sparkContext
-    # Other configs
-
-    # Other configs
-    # pd.options.display.float_format = "{:.2f}".format
-
     # Useful directory variables
-    # src_path = os.getcwd()
-    # root_path = os.path.dirname(src_path)
-    # data_path = root_path + "/datasets"
-    # visualization_path = root_path + "/data_visualization"
+    src_path = os.getcwd()
+    root_path = os.path.dirname(src_path)
+    data_path = root_path + "/datasets"
+    visualization_path = root_path + "/data_visualization"
+    consumption_train = pd.read_parquet(data_path+"/05_model_input/"+"consumption_train")
+    pressure_train = pd.read_parquet(data_path+"/05_model_input/"+"pressure_train")
+    #consumption_train[:-48*7]#Cantidad para entrenar
 
-    # Start counting time
-    start_t = time.time()
-    # Reading the original file
-    df = spark.read.parquet(
-        "output_final.parquet"
-    )  # Functional programming. Reading the raw data file with the Structured API
-    # df.printSchema()
-    df.createOrReplaceTempView("df")
-
-    # Generating a dataFrame with the times (every 30 minutes from start_timestamp to end_timestamp)
-    dates = pd.date_range(
-        start=datetime(2018, 1, 1, 0, 0, 0),
-        end=datetime(2021, 6, 30, 0, 0, 0),
-        freq="30min",
-    )
-    datetimes = [date.to_pydatetime() for date in dates]
-    time_df = (
-        spark.createDataFrame(datetimes, TimestampType())
-        .withColumnRenamed("value", "time")
-        .sort(F.asc("time"))
-    )
-
-    # Obtaning each consumption node in a list
-    node_list = (
-        spark.sql("SELECT node from df").rdd.flatMap(lambda x: x).collect()
-    )  # Getting the list with all the node names
-
-
-    # Obtaning each consumption node:
-    # We are having two time related Spark Dataframes that originally prior to iterate will be identical
-    # time_df: will remain unchanged during the whole execution, just a reference to ensure all the times are met and if not a null is given for the corresponding electrical consumption column
-    # consumption_df: this will suffer a left join at the end of each iteration and will be the container for all the consumption columns
-    consumption_df = spark.createDataFrame(time_df.rdd, time_df.schema)
-    consumption_df = consumption_df.withColumn("total_average_power_consumption_W", lit(0))
-    for node in node_list[:-3]:  # All the consumption related cluster nodes
-        sql_query_node_consumption = """
-                        SELECT 
-                            EXPLODE(power) as (time, node_{}_power_consumption) 
-                        FROM df
-                        WHERE 
-                            node LIKE "{}"
-                    """.format(
-            node, node
-        )
-        node_consumption = spark.sql(sql_query_node_consumption)
-        node_consumption = node_consumption.withColumn(
-            "time", F.to_timestamp(node_consumption.time, "yyyy-MM-dd HH:MM:SS")
-        )
-        node_consumption = node_consumption.groupBy(
-            "time", F.window("time", "30 minutes")
-        ).agg(
-            avg("node_{}_power_consumption".format(node)).alias(
-                "node_{}_power_consumption".format(node)
-            ),
-        )
-        node_consumption = node_consumption.select(
-            "time", "window.*", "node_{}_power_consumption".format(node)
-        ).sort(F.asc("time"))
-        node_consumption = node_consumption.select(
-            col("end").alias("time"), col("node_{}_power_consumption".format(node))
-        )
-        node_consumption = node_consumption.groupBy("time").agg(
-            avg("node_{}_power_consumption".format(node)).alias(
-                "node_{}_average_power_consumption".format(node)
-            )
-        )
-        node_consumption = node_consumption.select(
-            "time", "node_{}_average_power_consumption".format(node)
-        ).sort(F.asc("time"))
-        consumption_df = consumption_df.join(node_consumption, ["time"], how="left").sort(
-            F.asc("time")
-        )
-        consumption_df = consumption_df.fillna(0, subset=["node_{}_average_power_consumption".format(node)])
-        consumption_df = consumption_df.withColumn("total_average_power_consumption_W", col("total_average_power_consumption_W")+col("node_{}_average_power_consumption".format(node)))
-        consumption_df = consumption_df.drop("node_{}_average_power_consumption".format(node))
-   
-
-    consumption_df.write.parquet("example_overflow")
-    end_t = time.time()
-    print("Time in seconds " + str(end_t - start_t))
-
+    sarimax_model = SARIMAX(consumption_train[:-48*7].total_average_power_consumption_kW_chillers, order = (48, 0, 3), trend="c").fit()
+    with open(data_path+"/06_models/sarimax_consumption_48_0_3.pickle", "wb") as f: #For some reason, models in statsmodels.tsa.statespace have pending the implementation of .load()
+        pickle.dump(sarimax_model, f)
     spark.stop()
